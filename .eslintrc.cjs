5931cce3303cc9dfee940ceae8b253813cd1a9b6
Echo·Lin <1059845833@qq.com>
2022-11-23 17:48:43 +0800

    init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,6 +21,6 @@ module.exports = {
     'prettier/prettier': 'error',
     semi: ['error', 'always'], // 代码需要以分号结尾
     quotes: ['error', 'single'], // 单引号
-    'comma-dangle': 'ignore', // 不检查尾部额外的逗号
+    'comma-dangle': 'off', // 不检查尾部额外的逗号
   },
 };
